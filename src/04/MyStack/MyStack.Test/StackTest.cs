0a71557c1342990a76fc1eabeaf38f978163aedd
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2018-05-25 15:49:47 +0900

    4 | ソースを追加する

---
+++ src/04/MyStack/MyStack.Test/StackTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+using Xunit;
+
+namespace MyStack.Test
+{
+    public class StackTest
+    {
+        private Stack stack;
+
+        public StackTest()
+        {
+            stack = new Stack();
+        }
+
+        [Fact]
+        public void 作成直後は空()
+        {
+            Assert.True(stack.IsEmpty);
+        }
+
+        [Fact]
+        public void Pushしたら空でない()
+        {
+            stack.Push(1);
+            Assert.False(stack.IsEmpty);
+        }
+
+        [Fact]
+        public void Pushした値をTopで取得できる()
+        {
+            stack.Push(1);
+            Assert.Equal(1, stack.Top);
+        }
+
+        [Fact]
+        public void 一つPushするとSizeが1増える()
+        {
+            stack.Push(1);
+            Assert.Equal(1, stack.Size);
+            stack.Push(2);
+            Assert.Equal(2, stack.Size);
+        }
+    }
+}
---
+++ src/04/MyStack/MyStack/Stack.cs
@@ -0,0 +1,24 @@
+﻿namespace MyStack
+{
+    public class Stack
+    {
+        private int value;
+        private int size;
+
+        public Stack()
+        {
+        }
+
+        public bool IsEmpty => size == 0;
+
+        public int Top => value;
+
+        public int Size => size;
+
+        public void Push(int value)
+        {
+            this.value = value;
+            size++;
+        }
+    }
+}
