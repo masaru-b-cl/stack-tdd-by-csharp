00d48f31bfe51b17e68f7b64a283fcef2a49eb06
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2018-05-25 16:58:38 +0900

    5 | ソースを追加する

---
+++ src/05/MyStack/MyStack/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MyStack
+{
+    public class Stack
+    {
+        private int value;
+        private int size;
+
+        public Stack()
+        {
+        }
+
+        public bool IsEmpty => size == 0;
+
+        public int Top
+        {
+            get
+            {
+                AssertNotEmpty();
+                return value;
+            }
+        }
+
+        public int Size => size;
+
+        public void Push(int value)
+        {
+            this.value = value;
+            size++;
+        }
+
+        public void Pop()
+        {
+            AssertNotEmpty();
+        }
+
+        private void AssertNotEmpty()
+        {
+            if (IsEmpty) throw new InvalidOperationException();
+        }
+    }
+}
